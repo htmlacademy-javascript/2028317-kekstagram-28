624a860bd2207d21ecf2adeef7c32043f6fa0e3c
yuliazausailova <113924908+yuliazausailova@users.noreply.github.com>
2023-03-26 18:50:57 +0300

    Правки

--- js/similar-pictures.js
+++ js/similar-pictures.js
@@ -2,7 +2,8 @@ import { createPhotoDescriptions } from './data.js';
 
 const photoDescriptions = createPhotoDescriptions();
 
-const container = document.querySelector('.user-pictures');
+const container = document.querySelector('.pictures.container');
+console.log(container, "container");
 const template = document.querySelector('#picture').content.querySelector('.picture');
 
 photoDescriptions.forEach((description) => {
