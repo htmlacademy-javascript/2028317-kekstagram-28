be6d250e7f62f58a3b6922cf47a47b03adb41b86
Keks <keks@htmlacademy.ru>
2023-04-09 21:51:39 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/similar-pictures.js
+++ 11/js/similar-pictures.js
@@ -20,6 +20,20 @@ function onErrorModalButtonClick() {
   errorModal.classList.add('hidden');
 }
 
+const filterImages = debounce((filterType, images) => {
+  switch (filterType) {
+    case 'default':
+      prepareImages(images);
+      break;
+    case 'random':
+      prepareImages(images.sort(() => 0.5 - Math.random()).slice(0, RANDOM_IMAGES_COUNT));
+      break;
+    case 'discussed':
+      prepareImages(images.sort((a, b) => b.comments.length - a.comments.length));
+      break;
+  }
+}, FILTER_DEBOUNCE_DELAY);
+
 function prepareImages(images) {
   container.querySelectorAll('.picture').forEach((el) => el.remove());
   images.forEach((description) => {
@@ -40,9 +54,9 @@ window.addEventListener('load', () => {
     .then((images) => {
       prepareImages(images);
       imageListFilters.classList.remove('img-filters--inactive');
-      setFilterButtonClick(defaultFilterButton, debounce(() => prepareImages(images), FILTER_DEBOUNCE_DELAY));
-      setFilterButtonClick(randomFilterButton, debounce(() => prepareImages(images.sort(() => 0.5 - Math.random()).slice(0, RANDOM_IMAGES_COUNT)), FILTER_DEBOUNCE_DELAY));
-      setFilterButtonClick(discussedFilterButton, debounce(() => prepareImages(images.sort((a, b) => b.comments.length - a.comments.length)), FILTER_DEBOUNCE_DELAY));
+      setFilterButtonClick(defaultFilterButton, () => filterImages('default', images));
+      setFilterButtonClick(randomFilterButton, () => filterImages('random', images));
+      setFilterButtonClick(discussedFilterButton, () => filterImages('discussed', images));
     })
     .catch(() => {
       document.body.classList.add('modal-open');
