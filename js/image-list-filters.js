701d6858f236d111a2ac6e2e2d0c8b27b6511f9e
yuliazausailova <113924908+yuliazausailova@users.noreply.github.com>
2023-04-09 22:00:32 +0300

    Правки

--- js/image-list-filters.js
+++ js/image-list-filters.js
@@ -2,7 +2,6 @@ export function setFilterButtonClick(button, cb) {
   button.addEventListener('click', () => {
     document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
     button.classList.add('img-filters__button--active');
-    document.querySelector('.pictures.container').querySelectorAll('.picture').forEach((el) => el.remove());
     cb();
   });
 }
